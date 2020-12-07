4d8126cb001d1a913cba4293bbe75a662fcf425f
Pierre Navaro <navaro@math.cnrs.fr>
2020-12-07 10:46:48 +0100

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,20 @@
+from itertools import combinations
+from math import prod
+
+def read_puzzle(filename):
+    with open(filename) as f:
+         return list(map(int, f.readlines()))
+
+def ex1( puzzle ):
+    for item in combinations(puzzle, 2):
+        if sum(item) == 2020:
+            return prod(item)
+
+def ex2( puzzle ):
+    for item in combinations(puzzle, 3):
+        if sum(item) == 2020: 
+            return prod(item)
+
+puzzle = read_puzzle("input1.txt")
+print(ex1(puzzle))
+print(ex2(puzzle))
