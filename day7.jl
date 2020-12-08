8c6086bf532efcdd5a9292baed4f5ed9041a731d
Pierre Navaro <navaro@math.cnrs.fr>
2020-12-08 14:59:13 +0100

    Update day7.jl

--- day7.jl
+++ day7.jl
@@ -31,11 +31,26 @@ function hold_shiny_gold( color, bags )
     elseif length(bags[color]) > 0
        return any([hold_shiny_gold( c, bags ) for c in unique(bags[color])])
     else
-	return false
+	   return false
     end
+
+end
+
+function contained_bags( color, bags, n)
+
+    if length(bags[color]) == 0
+        return n
+    else
+        s = length(bags[color])
+        for c in bags[color]
+           s += contained_bags(c, bags, n)
+        end
+        return s
+    end
+
 
 end
 
 bags = load_data("input7.txt")
 println(count([hold_shiny_gold(c, bags) for c in keys(bags)]))
-
+println(contained_bags( "shiny gold", bags, 0))
