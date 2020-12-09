91dd6132fc05272b08c58a9d222c76672d5e6bac
Pierre Navaro <navaro@math.cnrs.fr>
2020-12-09 09:55:30 +0100

    Update day9.jl

--- day9.jl
+++ day9.jl
@@ -34,4 +34,25 @@ function part1(puzzle, preamble)
     end
 end
 
+function part2(puzzle, preamble)
+
+    numbers = load(puzzle)
+    size = length(numbers)
+    invalid = part1(puzzle, preamble)
+    deque = numbers[1:2]
+    s = sum(deque)
+    index = 3
+    while s !== invalid && index < size
+        if s < invalid
+           push!(deque, numbers[index])
+           index += 1
+        elseif s > invalid
+           popfirst!(deque)
+        end
+        s = sum(deque)
+    end
+    return maximum(deque) + minimum(deque)
+    
+end
 println(part1("input9.txt", 25))
+println(part2("input9.txt", 25))
