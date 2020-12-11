82b1b950325a8067ba5b7d23b624863a5068af91
Pierre Navaro <navaro@math.cnrs.fr>
2020-12-11 20:38:46 +0100

    Update day11.jl

--- day11.jl
+++ day11.jl
@@ -1,10 +1,17 @@
 function load( filename )
 
+    trans = Dict( "."=>0, "L"=>1,"#"=>2)
+
     open(filename) do f
 
-        data = readlines(f)
-        data = map(line -> (map( x->(x=='L'), collect(line))), data)
-        data = Int.(vcat(data'...))
+        data = []
+        for line in readlines(f)
+           line = replace(line, "."=> "0" )
+           line = replace(line, "L"=> "1" )
+           line = replace(line, "#"=> "2" )
+           push!(data, parse.(Int, collect(line)))
+        end
+        data = vcat(data'...)
         m, n = size(data)
         seats = zeros(Int, m+2, n+2)
         seats[2:m+1,2:n+1] .= data
@@ -47,27 +54,25 @@ function round!( seats )
 
     m, n = size(seats)
     old = copy(seats)
-    state = false
+    state = true
     for i in 2:m-1, j in 2:n-1
         if old[i,j] == 1 && all(adjacents(i,j, old) .< 2)
             seats[i,j] = 2
-            state = true
+            state = false
         end
         if old[i,j] == 2 && count(adjacents(i,j, old) .== 2) >= 4
             seats[i,j] = 1
-            state = true
+            state = false
         end
     end
     state
 end
-#    for i in 1:size(seats)[1], j in 1:size(seats)[2]
-#        if seats[i,j] == 2 && count(seats[adjacents(i,j,seats)] .==2) >= 4
-#            seats[i,j] == 1
-#        end
-#    end
 
 seats = load("input11.txt")
-
-while round!( seats)
-   println(count(seats .== 2))
+while true
+   if round!( seats)
+       println(count(seats .== 2))
+       break
+  end
 end
+
