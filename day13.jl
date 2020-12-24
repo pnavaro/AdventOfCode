73611781b23ca7d542574621355f7f62b5fe8f15
Pierre Navaro <navaro@math.cnrs.fr>
2020-12-24 12:19:46 +0100

    Update day13.jl

--- day13.jl
+++ day13.jl
@@ -18,35 +18,87 @@ function part1()
        
 end
 
-check( timestamp, departs ) = all( [ (timestamp+t) % bus_id == 0 for (bus_id,t) in departs])
+function check( timestamp, departs ) 
 
-function part2(departs)
+   for (bus_id,t) in departs
+      if (timestamp+t) % bus_id !== 0
+         return false
+      end
+   end
+   return true
+end
+
+function part2(start, departs)
 
     bus_ids = split(departs,",")
     
-    res = Tuple{Int,Int}[]
+    departs = Tuple{Int,Int}[]
     for (minute,bus_id) in enumerate(bus_ids)
         if !occursin( "x", bus_id) 
-            push!(res, (parse(Int,bus_id),minute-1))
+            push!(departs, (parse(Int,bus_id),minute-1))
         end
     end
 
-    timestamp = 0
-    while !check(timestamp, res)
-        timestamp += 1
+    # # Brute force solution
+    # timestamp = 0
+    # while !check(timestamp, res)
+    #     timestamp += 1
+    # end
+    sort!(departs)
+    reverse!(departs)
+
+    flag = true
+    buf_size = 10000
+    buffer = copy(departs)
+    while true
+
+        flag, res = check( start:(start+buf_size), buffer)
+        if flag 
+           return res
+        end
+        start += buf_size
+        buffer = copy(departs)
+
     end
 
-    println(timestamp)
-    return timestamp
 
+end
+
+# brute force
+function check( timestamp, departs)
+    for (bus_id, t) in departs
+        if (timestamp+t) % bus_id !== 0
+            return false
+        end
+    end
+    return true
+end
+
+function check( timestamps, departs )
+    
+    bus_id, t = popfirst!(departs)
+    res = filter( x -> ((x+t) % bus_id == 0), timestamps)
+
+    if length(res) == 0 
+       return (false, res)
+    end
+
+    if length(departs) == 0
+       println(res[1])
+       return true, res[1]
+    else
+       return check( res, departs)
+    end
+       
 end
 
 
 println(part1())
-_, departs = readlines("input_example.txt")
-@assert part2(departs) == 1068781
-@assert part2("17,x,13,19") == 3417
-@assert part2("67,7,59,61") == 754018
-@assert part2("67,x,7,59,61") == 779210
-@assert part2("67,7,x,59,61") == 1261476
-@assert part2("1789,37,47,1889") == 1202161486
+@assert part2(1, "17,x,13,19") == 3417
+@assert part2(1, "67,7,59,61") == 754018
+@assert part2(1, "67,x,7,59,61") == 779210
+@assert part2(1, "7,13,x,x,59,x,31,19") == 1068781
+@assert part2(1, "67,7,x,59,61") == 1261476
+@assert part2(1, "1789,37,47,1889") == 1202161486
+_, departs = readlines("input13.txt")
+@time println(part2(100000000000000, departs))
