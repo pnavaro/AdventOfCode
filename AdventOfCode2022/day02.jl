85b061cd0c0a3778b71dc605648ee837f1e00e35
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2022-12-03 17:50:55 +0100

    Day 2

---
+++ AdventOfCode2022/day02.jl
@@ -0,0 +1,56 @@
+filename = joinpath(@__DIR__, "input02.txt")
+
+
+function strategy1( p1, p2 )
+
+    score = first(p2) - 'W'
+    if ((p2 == "X" && p1 == "A")|(p2 == "Y" && p1 == "B")|(p2 == "Z" && p1 == "C"))
+        return score + 3
+    else
+        return score + 6 * ((p2 == "X" && p1 == "C")|(p2 == "Z" && p1 == "B")|(p2 == "Y" && p1 == "A"))
+    end
+
+end
+
+function ex1(filename)
+
+    score = 0
+    open(filename) do f
+         for line in eachline(f)
+             p1, p2 = split(line)
+             score += strategy1(p1, p2)
+         end
+    end
+    return score
+
+end
+
+function strategy2( p1, p2 )
+
+    offset = first(p1) - 'A'
+    if p2 == "X" # lose
+        play = 'X' + mod(offset+2,3)
+    elseif p2 == "Y" # draw
+        play = 'X' + mod(offset,3)
+    else # win
+        play = 'X' + mod(offset+1,3)
+    end
+    return strategy1(p1, string(play))
+
+end
+
+function ex2(filename)
+
+    score = 0
+    open(filename) do f
+         for line in eachline(f)
+             p1, p2 = split(line)
+             score += strategy2(p1, p2)
+         end
+    end
+    return score
+
+end
+
+println(ex1(filename))
+println(ex2(filename))
